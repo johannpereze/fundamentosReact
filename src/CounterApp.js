3c2559f10b9cf5350febf89808cceeff367447bd
johannpereze <sebas.jsp@gmail.com>
2021-11-06 08:36:45 -0500

    Vemos el primer hook use state

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,12 +1,20 @@
 //crea todo esto con el snippet rafcp
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(0);
+
+  const handleAdd = () => {
+    //setCounter(counter + 1)     //opc 1 teniendo ya el valor en una variable
+    setCounter((c) => c + 1);     //opc 2 si no tengo el valor en una variable, lo puedo llamar con un callback
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
+      <h2> {counter} </h2>
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
