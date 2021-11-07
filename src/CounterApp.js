d7f8ae936996120603869a36995b25f9c93ab14d
johannpereze <sebas.jsp@gmail.com>
2021-11-07 18:30:00 -0500

    Terminanos counter app

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,11 +3,19 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(value);
 
   const handleAdd = () => {
-    //setCounter(counter + 1)     //opc 1 teniendo ya el valor en una variable
-    setCounter((c) => c + 1);     //opc 2 si no tengo el valor en una variable, lo puedo llamar con un callback
+    setCounter(counter + 1); //opc 1 teniendo ya el valor en una variable
+    //setCounter((c) => c + 1);     //opc 2 si no tengo el valor en una variable, lo puedo llamar con un callback
+  };
+
+  const handleSubstract = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
   };
 
   return (
@@ -15,6 +23,8 @@ const CounterApp = ({ value }) => {
       <h1>CounterApp</h1>
       <h2> {counter} </h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>reset</button>
+      <button onClick={handleSubstract}>-1</button>
     </>
   );
 };
