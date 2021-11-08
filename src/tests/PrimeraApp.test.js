f0d2d86709994aa23368f68c4757ccb461e45701
johannpereze <sebas.jsp@gmail.com>
2021-11-08 17:50:08 -0500

    Hacemos pruebas de snapshots del subtitulo

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,26 +1,32 @@
 // import { render } from "@testing-library/react"
-import PrimeraApp from "../PrimeraApp"
-import {shallow} from 'enzyme';
+import PrimeraApp from "../PrimeraApp";
+import { shallow } from "enzyme";
 
-describe('Pruebas en PrimeraApp', () => {
-    //test sin enzyme
-    // test('Debe mostrar el mensaje "Hola Mundo"', () => {
-    //     const saludo = 'Hola Mundo'
-    //     const {getByText} = render(<PrimeraApp saludo={saludo} />)
-    //     wrapper.getByText()
-    //     expect(getByText(saludo)).toBeInTheDocument()
-    // })
+describe("Pruebas en PrimeraApp", () => {
+  //test sin enzyme
+  // test('Debe mostrar el mensaje "Hola Mundo"', () => {
+  //     const saludo = 'Hola Mundo'
+  //     const {getByText} = render(<PrimeraApp saludo={saludo} />)
+  //     wrapper.getByText()
+  //     expect(getByText(saludo)).toBeInTheDocument()
+  // })
 
-    //test con enzyme:
-    test('Debe de mostrar PrimeraApp correctamente', () => {
+  //test con enzyme:
+  test("Debe de mostrar PrimeraApp correctamente", () => {
+    const saludo = "Hola Mundo";
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />);
 
-        const saludo = 'Hola Mundo'
-        
-        const wrapper = shallow(<PrimeraApp saludo={saludo} />)
+    expect(wrapper).toMatchSnapshot();
+  });
 
-        expect(wrapper).toMatchSnapshot();
+  test("debe mostrar el subtitulo enviado por props", () => {
+    const saludo = "Hola Mundo";
+    const subtitulo = "Hola Mundo";
 
-    })
-    
-    
-})
+    const wrapper = shallow(
+      <PrimeraApp saludo={saludo} subtitulo={subtitulo} />
+    );
+    const textoParrafo = wrapper.find("p").text();
+    expect(textoParrafo).toBe(subtitulo);
+  });
+});
